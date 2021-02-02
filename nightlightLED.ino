f817a13a1b74f2f353382406f52415d1e8ff0a4b
RAMGOPAL GARUDKAR <ramgopal.garudkar2020@vit.ac.in>
2021-02-02 22:05:34 +0530

    SECOND COMMIT

---
+++ nightlightLED.ino
@@ -0,0 +1,17 @@
+int sensePin=0;
+int ledPin=9;
+
+
+void setup(){
+  analogReference(DEFAULT);
+  pinMode(ledPin,OUTPUT);
+  Serial.begin(9600);
+}
+
+
+void loop(){
+int val=analogRead(sensePin);
+  
+ if (val < 800) digitalWrite(ledPin,HIGH);
+  else digitalWrite(ledPin,LOW);
+}
